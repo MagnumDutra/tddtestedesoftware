f9d269db648efd54cab729e74958081843a77050
Magnum <magnumdutra@outlook.com>
2014-06-01 11:48:28 -0300

    Inicio do Projeto

---
+++ confereDigito/confereDigito/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace confereDigito
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.ReadKey();
+        }
+        public static void pontos()
+        { 
+
+        }
+    }
+}
---
+++ testPotz/testPotz/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace testPotz
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
