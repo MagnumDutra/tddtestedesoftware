6fe5c7cb72557039b3cf34721166ec49c5ba704d
Jose Cassio <cassinho303@hotmail.com>
2014-06-01 23:11:30 -0300

    Criando o projeto de teste e o primeiro teste

---
+++ TDDTeste/TDD/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TDD
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestPotz()
+        {
+            MeuPotz potz = new MeuPotz(306543219);
+            Assert.AreEqual(300, potz.pontos);
+        }
+    }
+}
